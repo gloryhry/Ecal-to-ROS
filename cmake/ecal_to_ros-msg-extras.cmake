09314dc68a4937626c3e3c8c4d547a19afbd66c9
Glory <gloryhry@stu.xjtu.edu.cn>
2020-08-29 12:00:11 +0800

    Initial commit

---
+++ cmake/ecal_to_ros-msg-extras.cmake
@@ -0,0 +1,2 @@
+set(ecal_to_ros_MESSAGE_FILES "msg/RadarDetection.msg;msg/RadarDetectionImage.msg;msg/RadarStatus.msg")
+set(ecal_to_ros_SERVICE_FILES "")
---
+++ cmake/ecal_to_ros-msg-paths.cmake
@@ -0,0 +1,4 @@
+# generated from genmsg/cmake/pkg-msg-paths.cmake.installspace.in
+
+_prepend_path("${ecal_to_ros_DIR}/.." "msg" ecal_to_ros_MSG_INCLUDE_DIRS UNIQUE)
+set(ecal_to_ros_MSG_DEPENDENCIES std_msgs)
